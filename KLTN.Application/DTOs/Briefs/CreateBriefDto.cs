b6cad28453fdc2583d4446c77faeafe5c2a957a9
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-10-10 21:09:29 +0700

    adjust brief table

--- KLTN.Application/DTOs/Briefs/CreateBriefDto.cs
+++ KLTN.Application/DTOs/Briefs/CreateBriefDto.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace KLTN.Application.DTOs.Briefs
 {
     public class CreateBriefDto
     {
+        public string Title {get; set; }
         public string Content { get; set; }
     }
 }
