be062ff3506d287b0b531ceb3889f55a2f4455d2
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-10-11 11:10:06 +0700

    add api to get transcript

---
+++ KLTN.Application/DTOs/ScoreStructures/ScoreDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLTN.Application.DTOs.ScoreStructures
+{
+    public class ScoreDetailDto
+    {
+        public string ScoreStructureId { get; set; }  
+        public string ColumnName { get; set; }        
+        public double? Value { get; set; }
+        public double Percent {  get; set; }
+        public string? ScoreId { get; set; }
+        public List<ScoreDetailDto> Children { get; set; } = new List<ScoreDetailDto>();  // Điểm của các cột con
+    }
+}
