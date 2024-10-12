391e8f52974663dadcf9ce05269787f7f4f30673
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-10-12 16:09:20 +0700

    base email service

---
+++ KLTN.Infrastructure/Mailing/EmailSettings.cs
@@ -0,0 +1,21 @@
+﻿using KLTN.Domain.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLTN.Infrastructure.Mailing
+{
+    public class SMTPEmailSettings : ISMTPEmailSettings
+    {
+        public string DisplayName { get; set; }
+        public bool EnableVerification { get; set; }
+        public string From { get; set; }
+        public bool UseSsl { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Host { get ; set ; }
+    }
+}
