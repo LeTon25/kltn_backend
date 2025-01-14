c2d16c71a4ce40a713b02110484c8056095d4305
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2025-01-14 10:22:21 +0700

    update README.md

--- KLTN.Domain/Entities/Interfaces/IDateTracking.cs
+++ KLTN.Domain/Entities/Interfaces/IDateTracking.cs
@@ -8,7 +8,7 @@ namespace KLTN.Domain.Entities.Interfaces
 {
     public interface IDateTracking
     {
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } 
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set;}
 
